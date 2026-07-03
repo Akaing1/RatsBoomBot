a904e8f0c3358085541c8c71deb35d0fed685fc4
Anthony <anthony.arcomona@gmail.com>
2026-07-03 16:05:24 -0400

    update README.md

--- bot/commands/counters.py
+++ bot/commands/counters.py
@@ -13,3 +13,12 @@ class CounterCommands(commands.Component):
         exploded_count = await self.bot.services.counters.increment_counter("explode")
 
         await ctx.send(f"Rat has exploded {exploded_count} times.")
+
+    @commands.command(name="reklop")
+    async def reklop(self, ctx: commands.Context):
+        if not self.bot.services:
+            return
+
+        reklop_count = await self.bot.services.counters.increment_counter("reklop")
+
+        await ctx.send(f"Reklop is a femboy o7! He has been with {reklop_count} guys!")
