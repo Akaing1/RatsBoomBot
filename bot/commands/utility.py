d871bd5908f5440f9e74badf1581916d0275d523
Anthony <anthony.arcomona@gmail.com>
2026-07-03 16:41:35 -0400

    add lucky command

--- bot/commands/utility.py
+++ bot/commands/utility.py
@@ -60,6 +60,42 @@ class UtilityCommands(commands.Component):
             return
 
     @commands.command()
+    async def lucky(self, ctx: commands.Context, user: twitchio.User = None):
+
+        lucky_percentage = random.randint(0, 100)
+
+        if lucky_percentage < 20:
+            if user is None:
+                await ctx.reply(f"{ctx.chatter.name} is unlucky! You are {lucky_percentage}% lucky. Don't gamble!")
+                return
+            await ctx.reply(
+                f"{ctx.chatter.name} thinks {user} is unlucky! They are {lucky_percentage}% lucky. Don't let them gamble!")
+            return
+
+        elif lucky_percentage < 50:
+            if user is None:
+                await ctx.reply(f"{ctx.chatter.name} is kinda unlucky today... Only {lucky_percentage}% lucky.")
+                return
+            await ctx.reply(
+                f"{ctx.chatter.name} thinks {user} is kinda unlucky today... Only {lucky_percentage}% lucky.")
+            return
+
+        elif lucky_percentage > 80:
+            if user is None:
+                await ctx.reply(f"{ctx.chatter.name} is {lucky_percentage}% lucky today! Go buy a lottery ticket!")
+                return
+            await ctx.send(
+                f"{ctx.chatter.name} thinks {user} is {lucky_percentage}% lucky today! Go buy a lottery ticket!")
+
+        else:
+            if user is None:
+                await ctx.reply(f"{ctx.chatter.name} is kinda lucky today... Only {lucky_percentage}% stinky.")
+                return
+            await ctx.reply(
+                f"{ctx.chatter.name} thinks {user} is kinda lucky today... Only {lucky_percentage}% stinky.")
+            return
+
+    @commands.command()
     async def lurk(self, ctx: commands.Context):
         await ctx.reply(f"{ctx.chatter.name} has been spotted by a human and scattered! See you soon!")
 
