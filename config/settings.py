57848e4d058af4abbadfc9fb249b274d041888b8
Anthony <anthony.arcomona@gmail.com>
2026-08-04 14:31:41 -0400

    remove unused config

--- config/settings.py
+++ config/settings.py
@@ -72,24 +72,6 @@ class Settings:
         "learning"
     ).strip().lower()
 
-    DAILY_REDEEM_TITLE = os.getenv(
-        "DAILY_REDEEM_TITLE",
-        "Steal some cheese"
-    )
-
-    FIRST_REDEEM_TITLE = os.getenv(
-        "FIRST_REDEEM_TITLE",
-        "first"
-    )
-
-    DAILY_REDEEM_BREAD = int(
-        os.getenv("DAILY_REDEEM_BREAD", "100")
-    )
-
-    FIRST_REDEEM_BREAD = int(
-        os.getenv("FIRST_REDEEM_BREAD", "250")
-    )
-
 
 settings = Settings()
 
