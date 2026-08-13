2cb538dff1d26f7ad5d3bed0fc88a705c496041a
Anthony <anthony.arcomona@gmail.com>
2026-08-12 21:24:20 -0400

    fix stream log service

--- config/version.py
+++ config/version.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 APP_NAME = "RatsBoomBot"
-APP_VERSION = "5.4.2"
+APP_VERSION = "5.4.3"
 
 PROJECT_DIRECTORY = Path(__file__).resolve().parent.parent
 DEPLOYMENT_STAMP_PATH = PROJECT_DIRECTORY / ".data" / "deployment.txt"
