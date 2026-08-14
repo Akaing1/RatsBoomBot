a6462baf83be3e3ee5744bbac508a60fb1d912d6
Anthony <anthony.arcomona@gmail.com>
2026-08-13 22:27:37 -0400

    init Milky_GalaxyVT profile

---
+++ bot/channels/milky_galaxyvt/profile.py
@@ -0,0 +1,43 @@
+from bot.channels.milky_galaxyvt.commands.general import MilkyGalaxyCommands
+from bot.channels.milky_galaxyvt.commands.points import MilkyGalaxyPointsCommands
+from bot.channels.milky_galaxyvt.profile_details import MILKY_GALAXYVT_COMMUNITY_MESSAGES, MILKY_GALAXYVT_POINTS, MILKY_GALAXYVT_RAID_MESSAGES, MILKY_GALAXYVT_REDEEMS, MILKY_GALAXYVT_TIMER_MESSAGES
+from bot.profiles import ChannelProfile, FeatureDefaults, GlobalCommandDefaults
+
+MILKY_GALAXYVT_PROFILE = ChannelProfile(
+    channel_name="Milky_GalaxyVT",
+    components=(MilkyGalaxyCommands,),
+    features=FeatureDefaults(
+        channel=False,
+        timers=False,
+        points=False,
+        redeems=False,
+        community_events=False,
+        raid_responses=False
+    ),
+    globals=GlobalCommandDefaults(
+        enabled=False,
+        points=False,
+        viewer_queue=False,
+        shoutouts=False,
+        socials=False,
+        settings=False,
+        hi=False,
+        choice=False,
+        kaboom=False,
+        stinky=False,
+        lucky=False,
+        smart=False,
+        lurk=False,
+        help=False,
+        explode=False,
+        reklop=False,
+        randy=False,
+        car=False,
+        kamikaze=False
+    ),
+    timer_messages=MILKY_GALAXYVT_TIMER_MESSAGES,
+    community_messages=MILKY_GALAXYVT_COMMUNITY_MESSAGES,
+    raid_messages=MILKY_GALAXYVT_RAID_MESSAGES,
+    redeems=MILKY_GALAXYVT_REDEEMS,
+    points=MILKY_GALAXYVT_POINTS
+)
---
+++ bot/channels/milky_galaxyvt/profile_details.py
@@ -0,0 +1,53 @@
+from bot.profiles import (
+    CommunityMessages,
+    PointsConfig,
+    PointsMessages,
+    RaidMessages,
+    RedeemConfig,
+    RedeemMessages
+)
+
+
+MILKY_GALAXYVT_TIMER_MESSAGES = (
+    "Timer Placeholder"
+)
+
+
+MILKY_GALAXYVT_COMMUNITY_MESSAGES = CommunityMessages(
+    follow="Thanks for following, {username}!",
+    subscription="Thanks for subscribing, {username}!",
+    resubscription="Thanks for subscribing for {months} months, {username}!"
+)
+
+
+MILKY_GALAXYVT_RAID_MESSAGES = RaidMessages(
+    incoming=(
+        "@{raider_name} raided with {viewer_count} {viewer_word}!"
+    )
+)
+
+
+MILKY_GALAXYVT_REDEEMS = RedeemConfig(
+    daily_title="Daily Points",
+    first_title="First",
+    daily_amount=100,
+    first_amount=250,
+    daily_double_chance=0.05,
+    claim_milestones=(10, 25, 50, 100, 250, 500, 1000),
+    messages=RedeemMessages(
+
+    )
+)
+
+
+MILKY_GALAXYVT_POINTS = PointsConfig(
+    command_name="points",
+    points_per_message=10,
+    message_cooldown_seconds=60,
+    gamble_win_chance=0.45,
+    duel_expiration_seconds=60,
+    messages=PointsMessages(
+
+    )
+)
+
