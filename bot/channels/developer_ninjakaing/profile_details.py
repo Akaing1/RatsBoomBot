ca98fd213882dfd7eb7c85e13247267a43c8890e
Anthony <anthony.arcomona@gmail.com>
2026-08-17 16:20:21 -0400

    setting up dev acct

--- bot/channels/developer_ninjakaing/profile_details.py
+++ bot/channels/developer_ninjakaing/profile_details.py
@@ -21,6 +21,12 @@ DEVELOPER_NINJAKAING_FIRST_CHAT_SHOUTOUTS = (
         message="The lead developer @{username} has entered the test environment! {channel_url}",
         native_shoutout=True
     ),
+    FirstChatShoutout(
+        user_id="1185298405",
+        username="randomuser1727",
+        # message="An explosive threat @{username} has entered the test environment! {channel_url}",
+        native_shoutout=True
+    )
 )
 
 
