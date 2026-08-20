eaa313ad1e4497d6e097f562b4770d928f075965
Anthony <anthony.arcomona@gmail.com>
2026-08-20 13:03:26 -0400

    add hbd command to meinya

--- bot/channels/meinya_yozakura/commands/general.py
+++ bot/channels/meinya_yozakura/commands/general.py
@@ -9,3 +9,9 @@ class MeinyaCommands(ChannelComponent):
     def __init__(self, bot, profile: ChannelProfile, broadcaster_id: str):
         super().__init__(bot, profile, broadcaster_id)
 
+    @commands.command(name="hbd")
+    async def friend_test(self, ctx: commands.Context) -> None:
+        if not await self.require_profile_channel(ctx):
+            return
+
+        await ctx.send("Happy Birthday Meinya!")
