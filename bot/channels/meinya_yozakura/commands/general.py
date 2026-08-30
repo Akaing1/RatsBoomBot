3ff6fc42cbd251fb4c46b2e5a4fc082ff2cb647c
Anthony <anthony.arcomona@gmail.com>
2026-08-30 15:10:38 -0400

    add throne command

--- bot/channels/meinya_yozakura/commands/general.py
+++ bot/channels/meinya_yozakura/commands/general.py
@@ -10,8 +10,15 @@ class MeinyaCommands(ChannelComponent):
         super().__init__(bot, profile, broadcaster_id)
 
     @commands.command(name="hbd")
-    async def friend_test(self, ctx: commands.Context) -> None:
+    async def hbd(self, ctx: commands.Context) -> None:
         if not await self.require_profile_channel(ctx):
             return
 
         await ctx.send("Happy Birthday Meinya!")
+
+    @commands.command(name="throne")
+    async def throne(self, ctx: commands.Context) -> None:
+        if not await self.require_profile_channel(ctx):
+            return
+
+        await ctx.send("If you would like to support my 2.0 model, head to my throne: https://throne.com/meinya/item/81c32121-0be2-44c6-b29f-be926b04b92b")
