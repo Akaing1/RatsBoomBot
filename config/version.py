978be26be6cd96263fd43c96976806b20aac7bed
Anthony Arcomona <33577017+Akaing1@users.noreply.github.com>
2026-08-31 14:29:36 +0000

    Add stream information commands (config/version.py)

--- config/version.py
+++ config/version.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 APP_NAME = "RatsBoomBot"
-APP_VERSION = "8.0.0"
+APP_VERSION = "8.1.0"
 
 PROJECT_DIRECTORY = Path(__file__).resolve().parent.parent
 DEPLOYMENT_STAMP_PATH = PROJECT_DIRECTORY / ".data" / "deployment.txt"
