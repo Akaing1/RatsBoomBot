ba3ba4117f9c7edfe064e3eced3276eaea359bdc
Anthony Arcomona <33577017+Akaing1@users.noreply.github.com>
2026-09-01 23:14:26 +0000

    Register complete Ahirman test cleanup migration

--- storage/migrations/__init__.py
+++ storage/migrations/__init__.py
@@ -18,6 +18,7 @@ from storage.migrations.v013_chatter_profiles import migrate as migrate_chatter_
 from storage.migrations.v014_remove_test_raid_records import migrate as migrate_remove_test_raid_records
 from storage.migrations.v015_passive_point_payouts import migrate as migrate_passive_point_payouts
 from storage.migrations.v016_remove_remaining_ahirman_test_record import migrate as migrate_remove_remaining_ahirman_test_record
+from storage.migrations.v017_remove_ahirman_test_run import migrate as migrate_remove_ahirman_test_run
 
 MigrationFunction = Callable[[Any], Awaitable[None]]
 
@@ -45,5 +46,6 @@ MIGRATIONS: tuple[Migration, ...] = (
     Migration(version=13, name="chatter_profiles", run=migrate_chatter_profiles),
     Migration(version=14, name="remove_test_raid_records", run=migrate_remove_test_raid_records),
     Migration(version=15, name="passive_point_payouts", run=migrate_passive_point_payouts),
-    Migration(version=16, name="remove_remaining_ahirman_test_record", run=migrate_remove_remaining_ahirman_test_record)
+    Migration(version=16, name="remove_remaining_ahirman_test_record", run=migrate_remove_remaining_ahirman_test_record),
+    Migration(version=17, name="remove_ahirman_test_run", run=migrate_remove_ahirman_test_run)
 )
