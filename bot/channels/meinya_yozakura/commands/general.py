9c510ab709b5e1a38a1f9c05c3cad0400c5adcc6
Anthony Arcomona <33577017+Akaing1@users.noreply.github.com>
2026-09-01 21:19:33 +0000

    Add Meinya Discord and YouTube aliases

--- bot/channels/meinya_yozakura/commands/general.py
+++ bot/channels/meinya_yozakura/commands/general.py
@@ -2,6 +2,7 @@ from twitchio.ext import commands
 
 from bot.channels.component import ChannelComponent
 from bot.profiles import ChannelProfile
+from bot.shared.commands.socials import send_discord_response, send_youtube_response
 
 
 class MeinyaCommands(ChannelComponent):
@@ -22,3 +23,17 @@ class MeinyaCommands(ChannelComponent):
             return
 
         await ctx.send("If you would like to support my 2.0 model, head to my throne: https://throne.com/meinya/item/81c32121-0be2-44c6-b29f-be926b04b92b")
+
+    @commands.command(name="discord")
+    async def discord(self, ctx: commands.Context) -> None:
+        if not await self.require_profile_channel(ctx):
+            return
+
+        await send_discord_response(self.bot, ctx)
+
+    @commands.command(name="youtube")
+    async def youtube(self, ctx: commands.Context) -> None:
+        if not await self.require_profile_channel(ctx):
+            return
+
+        await send_youtube_response(self.bot, ctx)
