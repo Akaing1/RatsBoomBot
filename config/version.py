420f4dc708d4c7adb6399069de88c0c4a96a42b2
Anthony Arcomona <33577017+Akaing1@users.noreply.github.com>
2026-09-02 01:59:19 +0000

    Bump version to 8.5.3

--- config/version.py
+++ config/version.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 APP_NAME = "RatsBoomBot"
-APP_VERSION = "8.5.2"
+APP_VERSION = "8.5.3"
 
 PROJECT_DIRECTORY = Path(__file__).resolve().parent.parent
 DEPLOYMENT_STAMP_PATH = PROJECT_DIRECTORY / ".data" / "deployment.txt"
