757a66f921d833f40a347538e4e79f2d6740e075
Anthony Arcomona <33577017+Akaing1@users.noreply.github.com>
2026-09-07 14:47:35 +0000

    Make points command groups case-insensitive

--- bot/channels/ninjakaing/commands/points.py
+++ bot/channels/ninjakaing/commands/points.py
@@ -12,7 +12,7 @@ class NinjakaingPointsCommands(ChannelComponent):
         super().__init__(bot, profile, broadcaster_id)
         self.handler = PointsCommandHandler(bot)
 
-    @commands.group(name="bread", invoke_fallback=True)
+    @commands.group(name="bread", invoke_fallback=True, case_insensitive=True)
     async def bread(self, ctx: commands.Context, target: LocalizedUser = None) -> None:
         if not await self.require_feature(ctx, FeatureName.POINTS):
             return
@@ -54,7 +54,7 @@ class NinjakaingPointsCommands(ChannelComponent):
 
         await self.handler.gamble(ctx, amount, "bread")
 
-    @bread.group(name="duel", invoke_fallback=True)
+    @bread.group(name="duel", invoke_fallback=True, case_insensitive=True)
     async def bread_duel(self, ctx: commands.Context, opponent: LocalizedUser = None, amount: str = None) -> None:
         if not await self.require_feature(ctx, FeatureName.POINTS):
             return
