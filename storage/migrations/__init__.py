a1816d4bdaa680cae2f5b7cfbc6454a0789b08e1
Anthony Arcomona <33577017+Akaing1@users.noreply.github.com>
2026-09-07 19:17:32 +0000

    Register Flag Bearer migration

--- storage/migrations/__init__.py
+++ storage/migrations/__init__.py
@@ -27,6 +27,7 @@ from storage.migrations.v022_seed_crafting_patch_note import migrate as migrate_
 from storage.migrations.v023_gambling_loss_totals import migrate as migrate_gambling_loss_totals
 from storage.migrations.v024_raid_gamble_consumables import migrate as migrate_raid_gamble_consumables
 from storage.migrations.v025_ancient_pact import migrate as migrate_ancient_pact
+from storage.migrations.v026_flag_bearers_will import migrate as migrate_flag_bearers_will
 
 MigrationFunction = Callable[[Any], Awaitable[None]]
 
@@ -64,4 +65,5 @@ MIGRATIONS: tuple[Migration, ...] = (
     , Migration(version=23, name="gambling_loss_totals", run=migrate_gambling_loss_totals)
     , Migration(version=24, name="raid_gamble_consumables", run=migrate_raid_gamble_consumables)
     , Migration(version=25, name="ancient_pact", run=migrate_ancient_pact)
+    , Migration(version=26, name="flag_bearers_will", run=migrate_flag_bearers_will)
 )
