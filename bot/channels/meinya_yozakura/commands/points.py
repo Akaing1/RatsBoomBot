f598de0ad6e1ee4eb38ddb72093a5b9bcecc0afa
Anthony Arcomona <33577017+Akaing1@users.noreply.github.com>
2026-09-07 14:47:39 +0000

    Make points command groups case-insensitive

--- bot/channels/meinya_yozakura/commands/points.py
+++ bot/channels/meinya_yozakura/commands/points.py
@@ -61,7 +61,7 @@ class MeinyaPointsCommands(ChannelComponent):
 
         await self.handler.roulette(ctx, color, amount, "petals")
 
-    @petals.group(name="duel", invoke_fallback=True)
+    @petals.group(name="duel", invoke_fallback=True, case_insensitive=True)
     async def petals_duel(self, ctx: commands.Context, opponent: LocalizedUser = None, amount: str = None) -> None:
         if not await self.require_feature(ctx, FeatureName.POINTS):
             return
