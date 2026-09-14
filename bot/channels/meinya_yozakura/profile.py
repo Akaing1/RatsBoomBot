758908061331191cbc3b8a9d2914c456feb35eb5
Anthony <anthony.arcomona@gmail.com>
2026-09-13 20:14:21 -0400

    Fix spacing

--- bot/channels/meinya_yozakura/profile.py
+++ bot/channels/meinya_yozakura/profile.py
@@ -8,9 +8,9 @@ MEINYA_PROFILE = ChannelProfile(
     components=(MeinyaCommands, MeinyaPointsCommands),
     protected_user_ids=(
         "1251948863",  # Ninjakaing
-        "486983829",  # WxlfiiX
-        "104646528",  # Brlp39
-        "123061475" # UDoubleT
+        "486983829",   # WxlfiiX
+        "104646528",   # Brlp39
+        "123061475"    # UDoubleT
     ),
     shared_counters_enabled=True,
     features=FeatureDefaults(
